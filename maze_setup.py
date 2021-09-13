16b8f094b3428b32441ee25398c4426e7c327bcf
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-09-13 13:41:15 -0500

    Start at any starting point

--- maze_setup.py
+++ maze_setup.py
@@ -1,16 +1,12 @@
 import pygame
 import random
 import time
+import numpy
 
-#import maze_generator
 from maze_generator import *
 
 pygame.init()
 
-size = 5
-
-screen = pygame.display.set_mode([size*60,size*60])
-screen.fill((255,255,255))
 
 ##maze = [[2, 1, 0, 0, 0],
 ##        [0, 0, 0, 1, 0],
@@ -19,24 +15,21 @@ screen.fill((255,255,255))
 ##        [0, 0, 1, 0, 3]]
 
 print(maze)
+size = numpy.size(maze,1)
 
-for r in range(0,size):
-    for c in range(0,size):
-        if maze[r][c] == 0:
-            color = (255,255,255)
-        elif maze[r][c] == 1:
-            color = (0,0,0)
-        elif maze[r][c] == 2:
-            color = (0,255,0)
-        elif maze[r][c] == 3:
-            color = (255,0,0)
-        x = r * 60
-        y = c * 60
-        pygame.draw.rect(screen, color, pygame.Rect(y,x,60,60))
-        pygame.display.flip()           
+screen = pygame.display.set_mode([size*60,size*60])
+screen.fill((255,255,255))       
 
-x = 0
-y = 0
+for i in range(0,size):
+    try: 
+        y = maze[i].index(3)
+        x = i
+        break
+    except:
+        continue
+print((x,y))
+#x = 0
+#y = 0
 while True:
     for r in range(0,size):
         for c in range(0,size):
@@ -44,16 +37,16 @@ while True:
                 color = (255,255,255)
             elif maze[r][c] == 1:
                 color = (0,0,0)
-            elif maze[r][c] == 2:
-                color = (0,255,0)
             elif maze[r][c] == 3:
+                color = (0,255,0)
+            elif maze[r][c] == 4:
                 color = (255,0,0)
             pygame.draw.rect(screen, color, pygame.Rect(c*60,r*60,60,60))
             pygame.display.flip()
-    pygame.draw.rect(screen,(0,0,255),pygame.Rect(x*60,y*60,60,60))
+    pygame.draw.rect(screen,(0,0,255),pygame.Rect(y*60,x*60,60,60))
     pygame.display.flip()
 
-    if maze[x][y] == 3:
+    if maze[x][y] == 4:
         break
     
     i = random.choice(['x','y'])
@@ -63,14 +56,14 @@ while True:
         y = y + random.choice((-1,1))
         
     if x < 0:
-        x = 0
+        x = x + 1
     if x == size:
         x = x - 1
 
     if y < 0:
-        y = 0
+        y = x + 1
     if y == size:
-        y = x - 1
+        y = y - 1
         
-    #time.sleep(1)
+    time.sleep(1)
 
