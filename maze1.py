65d5b3c5070529a40a6d40d6be611e347f281d04
Cody Nichoson <codynichoson@gmail.com>
2021-09-13 10:14:46 -0500

    Added simple maze list.

---
+++ maze1.py
@@ -0,0 +1,16 @@
+## This is our first maze code
+import numpy as np
+
+
+maze = [[2, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 1, 1, 0, 0],
+        [1, 0, 0, 0, 1],
+        [0, 0, 1, 0, 3]]
+
+print(maze[0])
+print(maze[1])
+print(maze[2])
+print(maze[3])
+print(maze[4])
+
