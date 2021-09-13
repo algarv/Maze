3b94b459d7bffcb4d2bc621e1beac962906fdc30
Andru Liu <andruliu2022@u.northwestern.edu>
2021-09-13 16:08:36 -0500

    Completed pygame integration

--- recursive-test.py
+++ recursive-test.py
@@ -1,12 +1,34 @@
 from maze1 import maze
 import random
+import pygame
+import time
+
+pygame.init()
 
 x = 0
 y = 0
 size = 5
 solution = [[0] * size for j in range(size)]
 
+screen = pygame.display.set_mode([size*60,size*60])
+screen.fill((255,255,255))  
+
 while True:
+    for r in range(0,size):
+        for c in range(0,size):
+            if maze[r][c] == 0:
+                color = (255,255,255)
+            elif maze[r][c] == 1:
+                color = (0,0,0)
+            elif maze[r][c] == 2:
+                color = (0,255,0)
+            elif maze[r][c] == 3:
+                color = (255,0,0)
+            pygame.draw.rect(screen, color, pygame.Rect(c*60,r*60,60,60))
+            pygame.display.flip()
+    pygame.draw.rect(screen,(0,0,255),pygame.Rect(x*60,y*60,60,60))
+    pygame.display.flip()
+
     i = random.choice(("x", "y"))
     print(i) # for debugging
 
@@ -52,4 +74,5 @@ while True:
             print(solution)
             break
 
+    time.sleep(.5)
 
