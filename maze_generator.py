bdb7aa4025f7e2281b575bf51a112b41286f47f9
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-09-14 09:28:35 -0500

    I don't know

--- maze_generator.py
+++ maze_generator.py
@@ -29,17 +29,30 @@ wall_list = [[B[0]+1, B[1]],
 
 wall_list_size = [0, 1, 2, 3]
 
-#Remove neighbors who are out of maze range
-print(f"Wall List: {wall_list}")
-for x in wall_list_size:
-    for y in range(2):
-        if wall_list[x][y] < 0 or wall_list[x][y] >= size:
-            wall_list.pop(x)
-            wall_list_size.pop()
+###Remove neighbors who are out of maze range
+##print(f"Wall List: {wall_list}")
+##for x in wall_list_size:
+##    for y in range(2):
+##        if wall_list[x][y] < 0 or wall_list[x][y] >= size:
+##            wall_list.pop(x)
+##            wall_list_size.pop()
+##print(f"Adjusted Wall List: {wall_list}")
+
+##suggestion##
+for coordinates in wall_list:
+    if coordinates[0] < 0 or coordinates[0]>size-1 or coordinates[1]<0 or coordinates[1]>size-1:
+        wall_list.remove(coordinates)
+
+for coordinates in wall_list:
+    if coordinates[0] < 0 or coordinates[0]>size-1 or coordinates[1]<0 or coordinates[1]>size-1:
+        wall_list.remove(coordinates)
+
 print(f"Adjusted Wall List: {wall_list}")
+##############
+
 
 #While the wall list is not empty:
-for x in range(10):
+for x in range(size*3):
 
     #   Randomly choose a wall C from the wall list
     C = wall_list[random.randint(0,len(wall_list)-1)]
@@ -84,22 +97,38 @@ for x in range(10):
     B = D
 
     #   Add the walls of D to the wall list
-    wall_list_size = [0,1,2]
+    #wall_list_size = [0,1,2]
     wall_list = [[D[0]+1, D[1]],
                 [D[0], D[1]+1],
                 [D[0]-1, D[1]],
                 [D[0], D[1]-1]]
 
-    #   Remove C from the wall list
-    for x in wall_list_size:
-        if wall_list[x][0] == C[0] and wall_list[x][1] == C[1]:
-            wall_list.pop(x)
+    ##suggestion##
+    for coordinates in wall_list:
+        if coordinates[0] < 0 or coordinates[0]>size-1 or coordinates[1]<0 or coordinates[1]>size-1:
+            wall_list.remove(coordinates)
 
+    for coordinates in wall_list:
+        if coordinates[0] < 0 or coordinates[0]>size-1 or coordinates[1]<0 or coordinates[1]>size-1:
+            wall_list.remove(coordinates)
 
+    for coordinates in wall_list:
+        if coordinates == C:
+            wall_list.remove(coordinates)
+    
+    print(f"Adjusted Wall List: {wall_list}")
+    ##############
 
-maze = [[1, 3, 1, 0],
-        [1, 0, 1, 0],
-        [0, 0, 0, 0],
-        [0, 1, 1, 4]]
+    #Remove C from the wall list
+    #for x in wall_list_size:
+    #    if wall_list[x][0] == C[0] and wall_list[x][1] == C[1]:
+    #        wall_list.pop(x)
+
+maze_list[C[0]][C[1]]=3
+maze = maze_list
+##maze = [[1, 3, 1, 0],
+##        [1, 0, 1, 0],
+##        [0, 0, 0, 0],
+##        [0, 1, 1, 4]]
 
 
